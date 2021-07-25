9f9e256c6f35ad80afbfb391757c50b536ebeb17
Summersby95 <james.summersby@gmail.com>
2021-07-25 16:27:22 +0100

    feat: create job model

--- jobs/models.py
+++ jobs/models.py
@@ -34,3 +34,18 @@ class JobTypes(models.Model):
     class Meta:
         verbose_name_plural = 'Job Types'
 
+
+class Job(models.Model):
+    """ Job Model """
+    job_title = models.CharField(max_length=255)
+    department = models.ForeignKey(Departments, null=True, on_delete=models.SET_NULL)
+    type = models.ForeignKey(JobTypes, null=True, on_delete=models.SET_NULL)
+    status = models.ForeignKey(JobStatus, null=True, on_delete=models.SET_NULL)
+    priority = models.ForeignKey(JobPriority, null=True, on_delete=models.SET_NULL)
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.job_title
+
+
