980c0b2d55b6293f9f5f180c12d74de17ed2387f
Summersby95 <james.summersby@gmail.com>
2021-07-25 18:55:59 +0100

    feat: create outstanding jobs view

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Job, JobSteps, JobTimes
+
+
+def outstanding_jobs(request):
+    """ View to see outstanding jobs for user """
+    
+    jobs = Job.objects.filter()
+
+    context = {
+        'jobs': jobs,
+    }
+
+    return render(request, 'jobs/job_table.html', context)
+
 
 # Create your views here.
