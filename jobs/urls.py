101049f032637c2d6ce296513b907f856b7cd18b
Summersby95 <james.summersby@gmail.com>
2021-07-25 22:18:57 +0100

    feat: create edit job view and url pattern

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.outstanding_jobs, name='outstanding_jobs'),
     path('<int:job_id>/', views.job_details, name='job_detail'),
     path('create_job/', views.create_job, name='create_job'),
+    path('edit_job/<int:job_id>/', views.edit_job, name='edit_job'),
 ]
