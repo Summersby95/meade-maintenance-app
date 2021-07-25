5b47597a410596694ab479c532faf88936ae1da8
Summersby95 <james.summersby@gmail.com>
2021-07-25 22:15:46 +0100

    feat: add crispy forms to installed apps

--- pallas/settings.py
+++ pallas/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'dashboard',
     'jobs',
     'ancillaries',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -151,6 +152,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
