ccbbb025cbe0473d3199f951f8c609f13c8fa05f
Summersby95 <james.summersby@gmail.com>
2021-07-25 16:28:39 +0100

    feat: create admin views for job models

---
+++ jobs/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import *
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = (
+        'job_title',
+        'department',
+        'status',
+        'created_on',
+    )
+
+
+class JobStepsAdmin(admin.ModelAdmin):
+    list_display = (
+        'job',
+        'step_number',
+        'step',
+        'completed',
+    )
+
+
+class JobTimesAdmin(admin.ModelAdmin):
+    list_display = (
+        'job',
+        'time_start',
+        'time_end',
+    )
+
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobTypes)
+admin.site.register(JobStatus)
+admin.site.register(JobPriority)
+admin.site.register(JobSteps, JobStepsAdmin)
+admin.site.register(JobTimes, JobTimesAdmin)
