c4df27f7968a6b6b87f9173644ca821a78bcd069
Summersby95 <james.summersby@gmail.com>
2021-07-28 18:11:59 +0100

    fix: remove expandable tables button from datatables

--- static/js/datatables.js
+++ static/js/datatables.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $("#jobsTable").DataTable({
-        responsive: true,
+        responsive: {
+            details: false
+        },
         columnDefs: [
             { responsivePriority: 1, targets: 1 },
             { responsivePriority: 2, targets: -1 },
