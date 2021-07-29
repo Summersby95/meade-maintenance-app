bd0dd746c796eca31d48665cd6da4cf83dd1fd6d
Summersby95 <james.summersby@gmail.com>
2021-07-29 15:36:38 +0100

    feat: create url patterns for log times and status update views

--- jobs/urls.py
+++ jobs/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('<int:job_id>/', views.job_details, name='job_detail'),
     path('create_job/', views.create_job, name='create_job'),
     path('edit_job/<int:job_id>/', views.edit_job, name='edit_job'),
+    path('log_time/<int:job_id>/', views.create_time_log, name='create_time_log'),
+    path('mark_completed/<int:job_id>/', views.mark_completed, name='mark_completed'),
+    path('reopen/<int:job_id>/', views.reopen_job, name='reopen_job'),
+    path('cancel/<int:job_id>/', views.cancel_job, name='cancel_job'),
 ]
