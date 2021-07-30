e0b4faa84befbe2ce697ddcb5f37487f151dd21f
Summersby95 <james.summersby@gmail.com>
2021-07-30 15:31:30 +0100

    feat: create profiles models and migrations

---
+++ profiles/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ancillaries.models import Departments
+
+
+class UserTypes(models.Model):
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        verbose_name_plural = "User Types"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    user_type = models.ForeignKey(UserTypes, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
