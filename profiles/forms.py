72eef6c61b164adda420de51bc3ce96f77d13eca
Summersby95 <james.summersby@gmail.com>
2021-08-02 22:07:02 +0100

    feat: create custom signup form

---
+++ profiles/forms.py
@@ -0,0 +1,26 @@
+from allauth.account.forms import SignupForm
+from django import forms
+from ancillaries.models import Departments
+from .models import *
+
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
+    department = forms.ModelChoiceField(queryset=Departments.objects.all(), required=True)
+    user_type = forms.ModelChoiceField(queryset=UserTypes.objects.all(), required=True)
+    phone_number = forms.CharField(max_length=15, required=True)
+    
+    def signup(self, request, user):
+        user.save()
+        profile = UserProfile(
+            user=user,
+            first_name=self.cleaned_data['first_name'],
+            last_name=self.cleaned_data['last_name'],
+            department=self.cleaned_data['department'],
+            user_type=self.cleaned_data['user_type'],
+        )
+        profile.save()
+        return user
+
+    
