c2f357bc9e1af4cc0a106fc7f6553db4fc9ec7a4
Summersby95 <james.summersby@gmail.com>
2021-08-07 15:49:20 +0100

    feat: add create project url pattern

--- projects/urls.py
+++ projects/urls.py
@@ -4,3 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.ongoing_projects, name='ongoing_projects'),
     path('<int:project_id>/', views.project_details, name='project_details'),
+    path('create_project/', views.create_project, name='create_project'),
