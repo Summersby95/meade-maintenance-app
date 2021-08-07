92484bedcda178a035e803803f52ac60e889ca55
Summersby95 <james.summersby@gmail.com>
2021-08-07 15:50:30 +0100

    feat: create edit project url pattern

--- projects/urls.py
+++ projects/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('', views.ongoing_projects, name='ongoing_projects'),
     path('<int:project_id>/', views.project_details, name='project_details'),
     path('create_project/', views.create_project, name='create_project'),
+    path('edit_project/<int:project_id>/', views.edit_project, name='edit_project'),
