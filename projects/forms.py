31753149b5c2659d0fa901f5b5154af1a9b89acf
Summersby95 <james.summersby@gmail.com>
2021-08-07 15:43:02 +0100

    feat: create project form form class

---
+++ projects/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Project
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class ProjectForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        exclude = [
+            'created_on',
+            'status',
+        ]
+        widgets = {
+            'due_date': DateInput(),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        self.profile = kwargs.pop('profile')
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        self.fields['created_by'].initial = self.profile.user.id
+        self.fields['created_by'].widget.attrs['hidden'] = True
+        self.fields['created_by'].label = ""
+        self.fields['created_by'].widget.attrs['readonly'] = True
