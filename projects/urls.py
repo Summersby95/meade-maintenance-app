0d9602eac798da276d2c3c8bd09210c810be88e3
Summersby95 <james.summersby@gmail.com>
2021-08-07 15:54:21 +0100

    feat: create cancel project url pattern

--- projects/urls.py
+++ projects/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('create_project/', views.create_project, name='create_project'),
     path('edit_project/<int:project_id>/', views.edit_project, name='edit_project'),
     path('mark_completed/<int:project_id>/', views.mark_project_completed, name='mark_project_completed'),
+    path('cancel/<int:project_id>', views.cancel_project, name='cancel_project'),
+]
