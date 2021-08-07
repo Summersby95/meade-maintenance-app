b9d55cdf6c6354b546e054c3476b7e604508b19a
Summersby95 <james.summersby@gmail.com>
2021-08-07 15:45:28 +0100

    feat: add context variables to project details view

--- projects/views.py
+++ projects/views.py
@@ -38,8 +38,28 @@ def ongoing_projects(request):
 def project_details(request, project_id):
     """ View to see project details. """
     project = get_object_or_404(Project, pk=project_id)
+    jobs = Job.objects.filter(project=project)
+
+    total_time = datetime.timedelta()
+    users = []
+    for job in jobs:
+        job_times = JobTimes.objects.filter(job=job)
+        for job_time in job_times:
+            total_time += job_time.time_end - job_time.time_start
+            if job_time.user.username not in users:
+                users.append(job_time.user.username)
+    
+    completed = sum(job.status.status == 'Completed' for job in jobs)
+    average_time = total_time / len(jobs) if len(jobs) > 0 else 0
+    distinct_users = len(users)
+
     context = {
         'project': project,
+        'jobs': jobs,
+        'total_time': total_time,
+        'completed': completed,
+        'average_time': average_time,
+        'distinct_users': distinct_users,
     }
     context = {**app_context, **context}
     return render(request, 'projects/project_details.html', context)
