c4dbabfab83462baab789536ea3e4971db35b3ec
Summersby95 <james.summersby@gmail.com>
2021-08-08 20:41:10 +0100

    feat: create ppm form

--- assets/forms.py
+++ assets/forms.py
@@ -17,3 +17,12 @@ class AssetForm(forms.ModelForm):
         super(AssetForm, self).__init__(*args, **kwargs)
 
 
+class PPMForm(forms.ModelForm):
+    
+    class Meta:
+        model = PPM
+        exclude = ['asset', 'created_by',]
+
+    def __init__(self, *args, **kwargs):
+        super(PPMForm, self).__init__(*args, **kwargs)
+        self.fields['time_interval'].label = 'Time Interval (Days)'
