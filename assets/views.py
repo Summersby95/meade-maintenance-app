f9fa251c697b3afc20315e8155363e4982541b6b
Summersby95 <james.summersby@gmail.com>
2021-08-08 15:36:07 +0100

    feat: create asset details view

--- assets/views.py
+++ assets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from .models import Assets, AssetTypes, PPM
+from jobs.models import Job
 
 app_context = {
     'nbar': 'assets',
@@ -24,3 +25,21 @@ def active_assets(request):
     context = {**app_context, **context}
 
     return render(request, 'assets/assets_table.html', context)
+
+
+@login_required
+def asset_details(request, asset_id):
+    """ View to see details of a specific asset """
+    asset = get_object_or_404(Assets, pk=asset_id)
+    ppms = PPM.objects.filter(asset=asset)
+    jobs = Job.objects.filter(asset=asset)
+    
+    context = {
+        'asset': asset,
+        'ppms': ppms,
+        'jobs': jobs,
+    }
+    context = {**app_context, **context}
+
+    return render(request, 'assets/asset_details.html', context)
+
