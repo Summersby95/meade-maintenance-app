355e6d4482a14c89d8baaf0034fd78684ff9384b
Summersby95 <james.summersby@gmail.com>
2021-08-08 12:36:37 +0100

    feat: create assets model

--- assets/models.py
+++ assets/models.py
@@ -13,3 +13,22 @@ class AssetTypes(models.Model):
         verbose_name_plural = 'Asset Types'
 
 
+class Assets(models.Model):
+    """
+    Assets model
+    """
+    asset_name = models.CharField(max_length=40)
+    asset_type = models.ForeignKey(AssetTypes, on_delete=models.CASCADE)
+    department = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    location = models.ForeignKey(Locations, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Suppliers, on_delete=models.CASCADE)
+    installation_date = models.DateField()
+    description = models.TextField()
+    barcode = models.CharField(max_length=20)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.asset_name
+    
+    class Meta:
+        verbose_name_plural = "Assets"
