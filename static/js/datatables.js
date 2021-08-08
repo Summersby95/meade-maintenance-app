0cdff021f45fd883696ea784c572e341bc4c7ea6
Summersby95 <james.summersby@gmail.com>
2021-08-08 12:31:43 +0100

    fix: remove resize function reload, change details parameter to true

--- static/js/datatables.js
+++ static/js/datatables.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
     $("#dataTable").DataTable({
         responsive: {
-            details: false
+            details: true
         },
         columnDefs: [
             { responsivePriority: 1, targets: 1 },
             { responsivePriority: 2, targets: -1 },
         ]
-    });
-    $(window).resize(function() {
-        window.location.reload();
     });
 });
