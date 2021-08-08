eddd3eff85db8fa6c39e8fa629eef2fdafbeb180
Summersby95 <james.summersby@gmail.com>
2021-08-08 20:40:41 +0100

    feat: create asset url patterns

--- assets/urls.py
+++ assets/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.active_assets, name='active_assets'),
     path('inactive/', views.inactive_assets, name='inactive_assets'),
     path('<int:asset_id>/', views.asset_details, name='asset_details'),
+    path('edit_asset/<int:asset_id>/', views.edit_asset, name='edit_asset'),
 ]
