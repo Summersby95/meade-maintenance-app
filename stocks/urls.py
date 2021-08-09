18a48a35e22f97cbc7c25c60e6bc89f6d410145b
Summersby95 <james.summersby@gmail.com>
2021-08-09 18:14:21 +0100

    feat: add transfer stock url pattern

--- stocks/urls.py
+++ stocks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:stock_id>/', views.stock_item_details, name='stock_item_details'),
     path('create_item/', views.create_stock_item, name='create_stock_item'),
     path('receive_stock/', views.create_stock_receipt, name='create_stock_receipt'),
+    path('transfer_stock/', views.create_stock_transfer, name='create_stock_transfer'),
 ]
