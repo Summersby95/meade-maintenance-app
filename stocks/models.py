d07dce9943b4dc78c702645a1159b4bb02b9a67d
Summersby95 <james.summersby@gmail.com>
2021-08-09 18:03:48 +0100

    feat: create stock item model

--- stocks/models.py
+++ stocks/models.py
@@ -29,3 +29,25 @@ class StockMastercode(models.Model):
         verbose_name_plural = 'Stock Mastercodes'
 
 
+class StockItem(models.Model):
+    """ Stock Item Model """
+    name = models.CharField(max_length=100)
+    mastercode = models.ForeignKey(StockMastercode, on_delete=models.SET_NULL, null=True)
+    location = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True)
+    barcode = models.CharField(max_length=20, null=True)
+    stock_alert = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_current_stock(self):
+        return (
+            (StockReceipts.objects.filter(item=self).aggregate(models.Sum('quantity'))['quantity__sum'] or 0)
+            -
+            (StockTransfer.objects.filter(item=self).aggregate(models.Sum('quantity'))['quantity__sum'] or 0)
+        )
+    
+    class Meta:
+        verbose_name = 'Stock Item'
+        verbose_name_plural = 'Stock Items'
+
