0a7de04e91baa98059d6545f1892fb1a7ab6a6ed
Summersby95 <james.summersby@gmail.com>
2021-08-09 18:07:28 +0100

    feat: create stock item details view

--- stocks/views.py
+++ stocks/views.py
@@ -25,3 +25,19 @@ def inventory_view(request):
 
     return render(request, 'stocks/inventory_table.html', context)
 
+
+@login_required
+def stock_item_details(request, stock_id):
+    item = get_object_or_404(StockItem, pk=stock_id)
+    stock_receipts = StockReceipts.objects.filter(item=item)
+    stock_transfers = StockTransfer.objects.filter(item=item)
+
+    context = {
+        'item': item,
+        'stock_receipts': stock_receipts,
+        'stock_transfers': stock_transfers,
+    }
+    context = {**context, **app_context}
+
+    return render(request, 'stocks/stock_item_details.html', context)
+
