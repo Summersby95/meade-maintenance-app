d949471a0c3d5739b240749220d1057667efa37a
Summersby95 <james.summersby@gmail.com>
2021-08-09 22:54:24 +0100

    feat: add create item stock receipt url pattern

--- stocks/urls.py
+++ stocks/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create_item/', views.create_stock_item, name='create_stock_item'),
     path('edit_item/<int:stock_id>/', views.edit_stock_item, name='edit_stock_item'),
     path('receive_stock/', views.create_stock_receipt, name='create_stock_receipt'),
+    path('receive_stock/item/<int:stock_id>/', views.create_item_stock_receipt, name='create_item_stock_receipt'),
     path('transfer_stock/', views.create_stock_transfer, name='create_stock_transfer'),
 ]
