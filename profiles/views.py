37afbba74cf6b2363e931aa21223d6628de84ea9
Summersby95 <james.summersby@gmail.com>
2021-08-10 18:22:46 +0100

    feat: add imports for profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,21 @@
+import datetime
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.db.models import Q, Sum
+from .models import UserProfile
+from jobs.models import Job, JobStatus, JobTimes
+from stocks.models import StockTransfer
+
+
+app_context = {
+    'nbar': 'people',
+    'links': [
+        {
+            'href': 'staff_list',
+            'text': 'Staff List',
+        }
+    ]
+}
+
 
 # Create your views here.
