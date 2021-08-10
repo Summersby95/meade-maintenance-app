62dcd5c5c99f37009cc93e9dd50e78db608d2dd1
Summersby95 <james.summersby@gmail.com>
2021-08-10 18:24:44 +0100

    feat: add get full name method to user profile model

--- profiles/models.py
+++ profiles/models.py
@@ -23,3 +23,6 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
