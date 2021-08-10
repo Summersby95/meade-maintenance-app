b2784087466be775a918a80fc52a041861d71172
Summersby95 <james.summersby@gmail.com>
2021-08-10 18:25:54 +0100

    feat: add staff detail view

--- profiles/views.py
+++ profiles/views.py
@@ -29,3 +29,61 @@ def staff_list(request):
 
     return render(request, 'profiles/staff_list.html', context)
 
+
+@login_required
+def staff_detail(request, staff_id):
+    employee = UserProfile.objects.get(id=staff_id)
+    jobs = Job.objects.filter(Q(assigned_to__in=[employee.user]) | Q(created_by=employee.user))
+    stock_withdrawls = StockTransfer.objects.filter(user=employee.user)
+    time_logs = JobTimes.objects.filter(user=employee.user)
+
+    cancelled_jobs = jobs.filter(status=JobStatus.objects.get(status='Cancelled')).count()
+    completed_jobs = jobs.filter(status=JobStatus.objects.get(status='Completed')).count()
+    started_jobs = jobs.filter(status=JobStatus.objects.get(status='Started')).count()
+    outstanding_jobs = jobs.filter(~Q(status=JobStatus.objects.get(status='Completed'))).count()
+
+    hours_today = sum(
+        [time.time_diff() for time in time_logs.filter(
+            time_start__date=datetime.date.today()
+        )], 
+        datetime.timedelta()
+    )
+    hours_week = sum(
+        [time.time_diff() for time in time_logs.filter(
+            time_start__date__range=[
+                datetime.date.today() - datetime.timedelta(days=7), 
+                datetime.date.today()
+            ]
+        )], 
+        datetime.timedelta()
+    )
+    hours_month = sum(
+        [time.time_diff() for time in time_logs.filter(
+            time_start__date__range=[
+                datetime.date.today() - datetime.timedelta(days=30), 
+                datetime.date.today()
+            ]
+        )], 
+        datetime.timedelta()
+    )
+    hours_all = sum([time.time_diff() for time in time_logs], datetime.timedelta())
+
+    context = {
+        'employee': employee,
+        'jobs': jobs,
+        'stock_withdrawls': stock_withdrawls,
+        'time_logs': time_logs,
+        'cancelled_jobs': cancelled_jobs,
+        'completed_jobs': completed_jobs,
+        'started_jobs': started_jobs,
+        'outstanding_jobs': outstanding_jobs,
+        'hours_today': hours_today,
+        'hours_week': hours_week,
+        'hours_month': hours_month,
+        'hours_all': hours_all,
+    }
+    context = {**app_context, **context}
+
+    return render(request, 'profiles/staff_detail.html', context)
+
+
