9d08356336265b927aa36b3365eef99fb1e8a6df
Summersby95 <james.summersby@gmail.com>
2021-08-11 00:00:39 +0100

    feat: add messages import to stocks app

--- stocks/views.py
+++ stocks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from ancillaries.models import Suppliers
 from assets.models import Assets
 from .models import StockItem, StockReceipts, StockTransfer
