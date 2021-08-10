73f907763d2bbb46f811d12980546a57986d6b4f
Summersby95 <james.summersby@gmail.com>
2021-08-10 18:16:07 +0100

    feat: create supplier form

--- ancillaries/forms.py
+++ ancillaries/forms.py
@@ -1,4 +1,17 @@
 from django import forms
+from .models import Suppliers
+
 
 class DateInput(forms.DateInput):
     input_type = 'date'
+
+
+class SupplierForm(forms.ModelForm):
+
+    class Meta:
+        model = Suppliers
+        fields = '__all__'
+    
+
+    def __init__(self, *args, **kwargs):
+        super(SupplierForm, self).__init__(*args, **kwargs)
