648957ff50f1c15da80fd76314139576e3cc8c87
Summersby95 <james.summersby@gmail.com>
2021-08-12 00:33:00 +0100

    feat: create user bonus url pattern

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.staff_list, name='staff_list'),
     path('<int:staff_id>/', views.staff_detail, name='staff_detail'),
+    path('bonus/<int:staff_id>', views.user_bonus, name='user_bonus'),
 ]
