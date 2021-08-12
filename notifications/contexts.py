7a3a5e8161e21237dbac30f47d25dd0e1b338480
Summersby95 <james.summersby@gmail.com>
2021-08-13 00:07:22 +0100

    feat: add notifications context processor

---
+++ notifications/contexts.py
@@ -0,0 +1,13 @@
+from .models import Notification
+
+
+def notification_count(request):
+    """
+    Returns the number of unread notifications for the user
+    """
+    notification_count = 0
+    if request.user.is_authenticated:
+        notification_count = Notification.objects.filter(
+            user=request.user, read=False).count()
+    
+    return {'notification_count': notification_count}
