e0745296f2067ed8dfe188f9e3b4977810f49aca
Summersby95 <james.summersby@gmail.com>
2021-08-12 17:52:57 +0100

    feat: create notification model

--- notifications/models.py
+++ notifications/models.py
@@ -14,3 +14,19 @@ class NotificationType(models.Model):
     class Meta:
         verbose_name_plural = "Notification Types"
 
+
+class Notification(models.Model):
+    """
+    Notification Model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.ForeignKey(NotificationType, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    value = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name_plural = "Notifications"
