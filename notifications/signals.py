7d4ff5b4cc3082d329c1b8cee5d8eb6fb9709e20
Summersby95 <james.summersby@gmail.com>
2021-08-13 17:51:20 +0100

    feat: add create notification function to signals file

---
+++ notifications/signals.py
@@ -0,0 +1,31 @@
+from django.db.models.signals import post_save, post_delete, m2m_changed
+from django.db.models import Q
+from django.dispatch import receiver
+from .models import Notification, NotificationType
+from jobs.models import Job
+from stocks.models import StockTransfer, StockReceipts, StockItem
+from profiles.models import UserTypes, UserProfile 
+
+
+def create_notification(user, notification_type, job=None, stock_item=None):
+    """
+    Function to create notification.
+    """
+    if notification_type == 'Job Alert':
+        if job:
+            count = Notification.objects.filter(user=user, job=job).count()
+            if count == 0:
+                Notification.objects.create(
+                    user=user,
+                    type=NotificationType.objects.get(type='New Job Alert'),
+                    job=Job.objects.get(id=job.id)
+                )
+    elif notification_type == 'Stock Alert':
+        if stock_item:
+            Notification.objects.create(
+                user=user,
+                type=NotificationType.objects.get(type='Stock Warning'),
+                stock_item=StockItem.objects.get(id=stock_item.id)
+            )
+
+
