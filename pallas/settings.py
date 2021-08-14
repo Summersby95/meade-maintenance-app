d7d5f483b73ef6c54616ddcdc3f10503dfb171e2
Summersby95 <james.summersby@gmail.com>
2021-08-14 22:15:11 +0100

    fix: add static root to settings file

--- pallas/settings.py
+++ pallas/settings.py
@@ -167,6 +167,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
