ad954317bd5536a9172722b57ab9fa13ce7b36e8
Summersby95 <james.summersby@gmail.com>
2021-08-14 22:04:33 +0100

    fix: remove stripe whsecret and public key from settings

--- pallas/settings.py
+++ pallas/settings.py
@@ -181,6 +181,4 @@ ACCOUNT_FORMS = {'signup': 'profiles.forms.CustomSignupForm'}
 LOGIN_REDIRECT_URL = 'dashboard'
 
 # Stripe
-STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
