1d97054c54ebb3b3e332cc09d0684bcf4381de3d
Summersby95 <james.summersby@gmail.com>
2021-08-14 22:51:31 +0100

    feat: update secret key and debug in settings

--- pallas/settings.py
+++ pallas/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ge9&-509ln=xn+c%886dfe6!%2#vv6%&q8g+25&z32qa#85=&1'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['meade-pallas.herokuapp.com', 'localhost']
 
