7063e6a6cb185990fea3352a1e9fe41cb91aa95e
Summersby95 <james.summersby@gmail.com>
2021-08-15 19:07:09 +0100

    fix: linting

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('', views.staff_list, name='staff_list'),
     path('<int:staff_id>/', views.staff_detail, name='staff_detail'),
     path('bonus/<int:staff_id>', views.user_bonus, name='user_bonus'),
-    path('bonus/<int:staff_id>/checkout_session/', views.create_checkout_session, name='create_checkout_session'),
-    path('bonus/<int:staff_id>/success/<slug:checkout_session_id>/', views.bonus_success, name='bonus_success'),
-    path('bonus/<int:staff_id>/cancel/', views.bonus_cancel, name='bonus_cancel'),
+    path('bonus/<int:staff_id>/checkout_session/',
+         views.create_checkout_session, name='create_checkout_session'),
+    path('bonus/<int:staff_id>/success/<slug:checkout_session_id>/',
+         views.bonus_success, name='bonus_success'),
+    path('bonus/<int:staff_id>/cancel/', views.bonus_cancel,
+         name='bonus_cancel'),
 ]
