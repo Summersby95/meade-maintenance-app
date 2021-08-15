642829ec1be766eb1c39ad2e69d53478467423c5
Summersby95 <james.summersby@gmail.com>
2021-08-15 19:05:46 +0100

    fix: linting

--- notifications/contexts.py
+++ notifications/contexts.py
@@ -9,5 +9,5 @@ def notification_count(request):
     if request.user.is_authenticated:
         notification_count = Notification.objects.filter(
             user=request.user, read=False).count()
-    
+
     return {'notification_count': notification_count}
