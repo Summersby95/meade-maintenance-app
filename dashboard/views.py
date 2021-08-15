64ace77310df1ab82986ffb3f22fedc6a24d24e7
Summersby95 <james.summersby@gmail.com>
2021-08-15 19:09:36 +0100

    feat: add statistics for dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -22,5 +22,61 @@ app_context = {
 @login_required
 def dashboard(request):
     """Returns dashboard home"""
-    context = app_context
+    jobs = {
+        'all': Job.objects.all().count(),
+        'completed': Job.objects.filter(
+            status=JobStatus.objects.get(status='Completed')
+        ).count(),
+        'started': Job.objects.filter(
+            status=JobStatus.objects.get(status='Started')
+        ).count(),
+        'cancelled': Job.objects.filter(
+            status=JobStatus.objects.get(status='Cancelled')
+        ).count(),
+    }
+    jobs['outstanding'] = jobs['all'] - jobs['completed'] - jobs['cancelled']
+
+    projects = {
+        'all': Project.objects.all().count(),
+        'completed': Project.objects.filter(
+            status=ProjectStatus.objects.get(status='Completed')
+        ).count(),
+        'project_jobs': Job.objects.filter(~Q(project=None)).count(),
+        'total_time_logged': sum(
+            [jt.time_diff() for jt in JobTimes.objects.filter(
+                ~Q(job__project=None)
+            )],
+            datetime.timedelta()
+        ),
+    }
+
+    people = {
+        'in_today': JobTimes.objects.filter(
+            time_start__date=datetime.datetime.now().date()
+        ).values('user').distinct().count(),
+        'hours_today': sum(
+            [jt.time_diff() for jt in JobTimes.objects.filter(
+                time_start__date=datetime.datetime.now().date()
+            )],
+            datetime.timedelta()
+        ),
+        'hours_week': sum(
+            [jt.time_diff() for jt in JobTimes.objects.filter(
+                time_start__range=(
+                    datetime.datetime.now() - datetime.timedelta(days=7),
+                    datetime.datetime.now()
+                )
+            )],
+            datetime.timedelta()
+        ),
+        'hours_month': sum(
+            [jt.time_diff() for jt in JobTimes.objects.filter(
+                time_start__range=(
+                    datetime.datetime.now() - datetime.timedelta(days=30),
+                    datetime.datetime.now()
+                )
+            )],
+            datetime.timedelta()
+        ),
+    }
     return render(request, 'dashboard/dashboard.html', context)
