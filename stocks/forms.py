77a20e06fbf8658072536c129cbb36f83760a08b
Summersby95 <james.summersby@gmail.com>
2021-08-15 19:08:16 +0100

    fix: linting

--- stocks/forms.py
+++ stocks/forms.py
@@ -1,13 +1,15 @@
 from django import forms
+
 from .models import StockItem, StockReceipts, StockTransfer
 from ancillaries.forms import DateInput
+
 
 class StockItemForm(forms.ModelForm):
 
     class Meta:
         model = StockItem
         fields = '__all__'
-    
+
     def __init__(self, *args, **kwargs):
         super(StockItemForm, self).__init__(*args, **kwargs)
 
@@ -19,7 +21,7 @@ class StockReceiptsForm(forms.ModelForm):
         widgets = {
             'date_received': DateInput()
         }
-    
+
     def __init__(self, *args, **kwargs):
         super(StockReceiptsForm, self).__init__(*args, **kwargs)
 
@@ -29,6 +31,6 @@ class StockTransferForm(forms.ModelForm):
     class Meta:
         model = StockTransfer
         exclude = ('user', 'created_on',)
-    
+
     def __init__(self, *args, **kwargs):
         super(StockTransferForm, self).__init__(*args, **kwargs)
