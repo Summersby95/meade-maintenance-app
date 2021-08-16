f0a1deaee1227b4936dd74fba67881385b0c723f
Summersby95 <james.summersby@gmail.com>
2021-08-16 22:09:04 +0100

    feat: stock test template tag

--- ancillaries/templatetags/auth_tests.py
+++ ancillaries/templatetags/auth_tests.py
@@ -12,3 +12,14 @@ def manager_test(context):
     else:
         return False
 
+
+@register.simple_tag(takes_context=True)
+def stock_test(context):
+    """ Template tag to check if user is a manager,
+    admin or stock controller """
+    u_type = context['profile'].user_type.type.lower()
+    if u_type in ('stock controller', 'admin', 'manager'):
+        return True
+    else:
+        return False
+
