42058f183b72f8f5066391443deb64d66f22104d
Summersby95 <james.summersby@gmail.com>
2021-08-19 14:44:53 +0100

    feat: add url patterns for activating, deactivating assets

--- assets/urls.py
+++ assets/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('<int:asset_id>/', views.asset_details, name='asset_details'),
     path('create_asset/', views.create_asset, name='create_asset'),
     path('edit_asset/<int:asset_id>/', views.edit_asset, name='edit_asset'),
+    path('edit_asset/inactive/<int:asset_id>/', views.make_asset_inactive,
+         name='make_asset_inactive'),
+    path('edit_asset/active/<int:asset_id>/', views.make_asset_active,
+         name='make_asset_active'),
     path('add_ppm/<int:asset_id>/', views.add_ppm, name='add_ppm'),
     path('ppm/<int:ppm_id>/', views.ppm_details, name='ppm_details'),
 ]
